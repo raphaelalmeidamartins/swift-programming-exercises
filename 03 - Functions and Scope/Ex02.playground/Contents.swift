c605d22df45002f82b7279688aaea09b3e142792
Raphael Martins <64360469+raphaelalmeidamartins@users.noreply.github.com>
2023-05-22 23:33:41 -0300

    :sparkles: feat: exercise 2 about funtions and scope

---
+++ 03 - Functions and Scope/Ex02.playground/Contents.swift
@@ -0,0 +1,31 @@
+//Don't change this code:
+func calculator() {
+  let a = 3 //First input
+  let b = 4 //Second input
+  
+  add(n1: a, n2: b)
+  subtract(n1: a, n2: b)
+  multiply(n1: a, n2: b)
+  divide(n1: a, n2: b)
+  
+}
+
+//Write your code below this line to make the above function calls work.
+
+func add(n1: Int, n2: Int) {
+    print(n1 + n2)
+}
+
+func subtract(n1: Int, n2: Int) {
+    print(n1 - n2)
+}
+
+func multiply(n1: Int, n2: Int) {
+    print(n1 * n2)
+}
+
+func divide(n1: Int, n2: Int) {
+    print(Float(n1) / Float(n2))
+}
+
+calculator()
