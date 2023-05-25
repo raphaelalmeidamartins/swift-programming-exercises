f9cd232d7373e404bec18ea2362069714496fe6c
Raphael Martins <64360469+raphaelalmeidamartins@users.noreply.github.com>
2023-05-25 20:34:08 -0300

    :sparkles: feat: dict exercise

---
+++ 04 - Conditionals/EX03.playground/Contents.swift
@@ -0,0 +1,21 @@
+func exercise() {
+
+    //Don't change this
+    var stockTickers: [String: String] = [
+        "APPL" : "Apple Inc",
+        "HOG": "Harley-Davidson Inc",
+        "BOOM": "Dynamic Materials",
+        "HEINY": "Heineken",
+        "BEN": "Franklin Resources Inc"
+    ]
+    
+    //Write your code here.
+    stockTickers["WORK"] = "Slack Technologies Inc"
+    stockTickers["BOOM"] = "DMC Global Inc"
+
+  
+     //Don't modify this
+    print(stockTickers["WORK"]!)
+    print(stockTickers["BOOM"]!)
+}
+ 
